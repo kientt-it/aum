2bd0fd07cf5a035a58d16a3e83372e9839b2db71
Trung Kien <kientt.info@gmail.com>
2024-11-07 16:53:38 +0700

    update

---
+++ test/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'Test',
+    'version': '1.0',
+    'summary': 'Module Test Odoo',
+    'author': 'Trung Kien',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        # 'views/log_record_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
+    
